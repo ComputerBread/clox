fde859bc783ddb2400f7fb0359b3a81ffea4a7e3
ComputerBread <contact.computerbread@gmail.com>
2024-06-25 18:11:36 +0200

    scanner

--- compiler.c
+++ compiler.c
@@ -5,4 +5,22 @@
 
 void compile(const char* source) {
     initScanner(source);
+    int line = -1;
+    for (;;) {
+        Token token = scanToken();
+        if (token.line != line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        } else {
+            printf("   | ");
+        }
+
+        // That %.*s in the format string is a neat feature. Usually, you set
+        // the output precision—the number of characters to show—by placing a
+        // number inside the format string. Using * instead lets you pass the
+        // precision as an argument. 
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+        if (token.type == TOKEN_EOF) break;
+    }
 }
