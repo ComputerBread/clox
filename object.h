beb902883f3246da48862cf860f3191300ef2ad4
ComputerBread <contact.computerbread@gmail.com>
2024-07-02 18:17:29 +0200

    function object

--- object.h
+++ object.h
@@ -2,16 +2,20 @@
 #define clox_object_h
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value)        (AS_OBJ(value)->type)
 
 #define IS_STRING(value)       isObjType(value, OBJ_STRING)
+#define IS_FUNCTION(value)     isObjType(value, OBJ_FUNCTION)
 
 #define AS_STRING(value)       ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value))->chars)
+#define AS_FUNCTION(value)     (((ObjFunction*)AS_OBJ(value)))
 
 typedef enum {
+  OBJ_FUNCTION,
   OBJ_STRING,
 } ObjType;
 
@@ -23,6 +27,15 @@ struct Obj {
   // useful for garbage collector!
   struct Obj* next;
 };
+
+// Each function has its own Chunk of code & other metadata
+// Functions are first class in Lox
+typedef struct {
+  Obj obj;
+  int arity; // nb of parameters the func expects
+  Chunk chunk;
+  ObjString* name; // function's name, to have better errors
+} ObjFunction;
 
 // Given an ObjString* you can safely cast if to Obj* (because Obj is the first
 // field). Every ObjString "is" an Obj in the OOP sense.
@@ -36,6 +49,8 @@ struct ObjString {
     uint32_t hash;
 };
 
+// to create a new Lox function
+ObjFunction* newFunction();
 
 static inline bool isObjType(Value value, ObjType type) {
     return IS_OBJ(value) && AS_OBJ(value)->type == type;
